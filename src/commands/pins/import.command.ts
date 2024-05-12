3b85931e591b5686dcafffe788caa46e9cb8fe0f
Sergio Barril <tropped@gmail.com>
2024-05-12 22:59:27 +0200

    Refactor and import command

---
+++ src/commands/pins/import.command.ts
@@ -0,0 +1,80 @@
+import {
+  ChannelType,
+  CommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+import { Command } from "../../interfaces/command";
+import { handlePinMessage } from "../../utils/pin.util";
+import logger from "../../config/logger";
+
+const data = new SlashCommandBuilder()
+  .setName("import")
+  .setDescription("Import existing pins from a channel")
+  .addChannelOption((option) =>
+    option
+      .setName("channel")
+      .setDescription("The channel to import the pins from")
+      .setRequired(true)
+      .addChannelTypes(ChannelType.GuildText),
+  );
+
+async function execute(interaction: CommandInteraction) {
+  await interaction.deferReply({ ephemeral: true });
+  const channelId = interaction.options.get("channel", true).channel?.id;
+
+  if (!interaction.inCachedGuild()) {
+    interaction.editReply("Interaction is not in a guild");
+    return;
+  }
+
+  if (!channelId) {
+    interaction.editReply("Please provide a valid channel");
+    return;
+  }
+  const { guild } = interaction;
+  const channel = await guild.channels.fetch(channelId);
+
+  if (!channel || !channel.isTextBased()) {
+    interaction.editReply("Channel not found");
+    return;
+  }
+
+  const pinnedMessages = await channel.messages.fetchPinned();
+
+  logger.info(
+    { pinnedMessages, channel },
+    "Importing pinned messages from channel",
+  );
+
+  if (!pinnedMessages.size) {
+    interaction.editReply("No pinned messages found in the channel");
+    return;
+  }
+
+  const pinnerId = interaction.user.id;
+
+  const results = await Promise.allSettled(
+    pinnedMessages.map((message) => handlePinMessage(pinnerId, message)),
+  );
+
+  const successfulPins = results.filter(
+    (result) => result.status === "fulfilled",
+  );
+
+  const failedPins = results.filter((result) => result.status === "rejected");
+
+  let replyMessage = `Imported ${successfulPins.length} pins.`;
+
+  if (failedPins.length) {
+    replyMessage += `\nFailed to import ${failedPins.length} pins.`;
+  }
+
+  await interaction.editReply(replyMessage);
+}
+
+const ping: Command = {
+  data,
+  execute,
+};
+
+export default ping;
--- src/context-menus/messages/pin-message.context.ts
+++ src/context-menus/messages/pin-message.context.ts
@@ -2,78 +2,13 @@ import {
   ApplicationCommandType,
   ContextMenuCommandBuilder,
   MessageContextMenuCommandInteraction,
-  userMention,
-  time,
-  TimestampStyles,
-  Message,
 } from "discord.js";
 import { ContextMenu } from "../../interfaces/context-menu";
-
-import GuildService from "../../services/guild.service";
-
-import logger from "../../config/logger";
-import { pinService } from "../../services/pin.service";
-import DuplicatePinError from "../../errors/duplicate-pin.error";
-import GuildChannelError from "../../errors/guild-channel.error";
-import PinChannelNotFoundError from "../../errors/pins-channel-not-found.error";
-import { pinAttachmentService } from "../../services/pin-attachment.service";
-import database from "../../database";
-import { getDiscordGuild } from "../utils/context-menu.utils";
-
-const guildService = new GuildService(database);
-
-const MAX_PINNED_MESSAGES = 50;
+import { handlePinMessage } from "../../utils/pin.util";
 
 const data = new ContextMenuCommandBuilder()
   .setName("Pin Message")
   .setType(ApplicationCommandType.Message);
-
-function prepareCloneMessage(pinnerId: string, targetMessage: Message) {
-  const { createdAt, author, content, attachments } = targetMessage;
-
-  const attachmentArray = Array.from(attachments.values());
-
-  let cloneContent = `👤 ${userMention(author.id)}\n`;
-  cloneContent += `🕒 ${time(createdAt, TimestampStyles.ShortDateTime)}\n`;
-  cloneContent += `📌 ${userMention(pinnerId)}\n`;
-  cloneContent += `📨 ${targetMessage.url}\n\n${content}`;
-
-  return {
-    content: cloneContent,
-    files: attachmentArray,
-    flags: "SuppressEmbeds" as const,
-  };
-}
-
-async function storeAttachments(attachments: string[], pinId: string) {
-  return Promise.all(
-    attachments.map((url) =>
-      pinAttachmentService.addPinAttachment({
-        pinId,
-        attachmentUrl: url,
-      }),
-    ),
-  ).catch((error) => {
-    logger.error(error, "Error storing attachment");
-  });
-}
-
-async function pinMessage(targetMessage: Message<boolean>) {
-  const { channel, pinnable, pinned } = targetMessage;
-  const channelPins = await channel.messages.fetchPinned();
-
-  const shouldBePinned =
-    channelPins.size < MAX_PINNED_MESSAGES && pinnable && !pinned;
-
-  if (shouldBePinned) {
-    await targetMessage.pin();
-    logger.info({ targetMessage }, "Message truly pinned");
-  } else {
-    logger.warn({ targetMessage }, "Message not pinned");
-  }
-
-  return shouldBePinned;
-}
 
 /**
  * Pin a message to the pins channel
@@ -81,71 +16,10 @@ async function pinMessage(targetMessage: Message<boolean>) {
 async function execute(interaction: MessageContextMenuCommandInteraction) {
   await interaction.deferReply({ ephemeral: true });
 
-  const discordGuild = getDiscordGuild(interaction);
-  const guildId = discordGuild.id;
-
-  const guild = await guildService.getGuildByDiscordId(guildId);
-  if (!guild.channelId) {
-    logger.error(guild, "Guild channel not assigned");
-    throw new GuildChannelError();
-  }
-
-  // Check if the message is from pinned channel
-  const { targetId, targetMessage } = interaction;
-
-  if (guild.channelId === targetMessage.channelId) {
-    logger.error({ interaction, targetMessage }, "Message already pinned");
-    interaction.editReply({
-      content: "Can't pin a message from the pins channel.",
-    });
-    return;
-  }
-
-  const oldPin = await pinService.findPinByMessageId(targetId);
-
-  if (oldPin) {
-    logger.warn({ interaction, oldPin }, "Pin already exists");
-    throw new DuplicatePinError();
-  }
-
-  // Get the channel for pinned messages
-  const pinsChannel = await discordGuild.channels.fetch(guild.channelId);
-
-  if (!pinsChannel || !pinsChannel.isTextBased()) {
-    logger.error(guild, "Pins channel not found");
-    throw new PinChannelNotFoundError(guild.channelId);
-  }
-
-  // Send the clone message to the pins channel
-  const pinnerId = interaction.user.id;
-  const clonedMessage = await pinsChannel.send(
-    prepareCloneMessage(pinnerId, targetMessage),
+  const clonedMessage = await handlePinMessage(
+    interaction.user.id,
+    interaction.targetMessage,
   );
-
-  // Prepare the pin data
-  const { createdAt, author, content, channelId } = targetMessage;
-  const pin = await pinService.createPin({
-    guildId: guild.id,
-    createdAt,
-    content,
-    channelId,
-    pinChannelId: guild.channelId,
-    authorId: author.id,
-    messageId: targetId,
-    discordId: clonedMessage.id,
-    pinnedBy: pinnerId,
-  });
-
-  logger.info({ pin }, "Message pinned");
-
-  // Store the attachments
-  const attachmentUrls = targetMessage.attachments.map(
-    (attachment) => attachment.url,
-  );
-  await storeAttachments(attachmentUrls, pin.id);
-
-  // Pin message for real
-  await pinMessage(targetMessage);
 
   await interaction.editReply({
     content: `Message pinned: ${clonedMessage.url}`,
