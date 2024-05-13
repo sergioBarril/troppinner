558c42fcebfdbbf13f996931ed8250a4d2f1d25d
Sergio Barril <tropped@gmail.com>
2024-05-13 23:44:30 +0200

    Remove ping commands

--- src/buttons/ping/ping.button.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonInteraction } from "discord.js";
-
-export default {
-  data: { name: "ping" },
-  async execute(interaction: ButtonInteraction) {
-    await interaction.reply("Pong button!");
-  },
-};
